3997e34213b1867f0059bd7f1ab11dfa67e25c3a
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-01-17 10:15:41 -0500

    api 8

--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,17 +1,18 @@
-/* import { DOMSelectors } from "./DOM";
+import { DOMSelectors } from "./DOM";
 import "../styles/style.css";
-import { apiLinks } from "./DOM";
+// import { apiLinks } from "./DOM";
 
-console.log(fetch(apiLinks.api));
+const api = "https://valorant-api.com/v1/agents";
+console.log(fetch(api));
 
 // fetch() returns a "response", which we must convert into a object json format
-fetch(apiLinks.api)
+fetch(api)
   .then((response) => response.json()) // use the `.json()` method
   .then((data) => console.log(data)); // `.json()` is also async, chain another `.then()` to log the object
 
 async function fetchData(api) {
   try {
-    const response = await fetch(apiLinks.api);
+    const response = await fetch(api);
     const data = await response.json();
     console.log(data);
     return data;
@@ -26,6 +27,9 @@ const apiResponseDOM = document.getElementById("api-response");
 const putcharacterInHTML = async () => {
   // defining an async arrow function
   const character = await fetchData(api);
-  apiResponseDOM.innerHTML = `character:`;
+  apiResponseDOM.innerHTML = ` <div class="card-info" id="card">
+  <h2>Agent:${api.displayName}</h2>
+  <q>Description: ${api.description}</q>
+  </div>`;
 };
-putcharacterInHTML(); */
+putcharacterInHTML();
