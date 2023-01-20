0b8882972eda57e73ca44bf248e0a0027284bf5f
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-01-20 10:16:24 -0500

    api 11

--- api-project/js/DOM.js
+++ api-project/js/DOM.js
@@ -2,6 +2,7 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("card"),
   input: document.getElementById("Searchinput"),
+  agent: document.querySelector("#displayName"),
 };
 
 export { DOMSelectors };
--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,14 +1,17 @@
 import { DOMSelectors } from "./DOM";
 import "../styles/style.css";
-// import { apiLinks } from "./DOM";
 
 const api = "https://valorant-api.com/v1/agents";
 console.log(fetch(api));
 
+const agent = document.querySelector("#displayName");
+const AgentSearch = `${api}${agent}`;
+console.log(AgentSearch);
+
 // fetch() returns a "response", which we must convert into a object json format
 fetch(api)
-  .then((response) => response.json()) // use the `.json()` method
-  .then((data) => console.log(data)); // `.json()` is also async, chain another `.then()` to log the object
+  .then((response) => response.json())
+  .then((data) => console.log(data));
 
 async function fetchData(api) {
   try {
@@ -22,11 +25,9 @@ async function fetchData(api) {
 }
 fetchData(api);
 
-// paired with DOM selectors, you can display dynamic data onto your HTML!
 DOMSelectors.button.addEventListener("click", function (event) {
   const apiResponseDOM = document.getElementById("api-response");
   const putcharacterInHTML = async () => {
-    // defining an async arrow function
     const character = await fetchData(api);
     apiResponseDOM.innerHTML = ` <div class="card-info" id="card">
   <h2 class="AgentName"> Agent: ${api.displayName} </h2>
