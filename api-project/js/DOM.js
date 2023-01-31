a29ec921c7e0c7e96845fb53e4306b86c84791c0
MinulGamage <113190129+MinulGamage@users.noreply.github.com>
2023-01-30 22:53:50 -0500

    api

--- api-project/js/DOM.js
+++ api-project/js/DOM.js
@@ -2,7 +2,8 @@ const DOMSelectors = {
   button: document.getElementById("btn"),
   box: document.getElementById("card"),
   input: document.getElementById("Searchinput"),
-  agent: document.querySelector("#displayName"),
+  valorant_html: document.querySelector("#displayName"),
+  Api: document.getElementById("https://valorant-api.com/v1/agents"),
 };
 
 export { DOMSelectors };
--- api-project/js/index.js
+++ api-project/js/index.js
@@ -1,38 +1,45 @@
 import { DOMSelectors } from "./DOM";
 import "../styles/style.css";
 
-const api = "https://valorant-api.com/v1/agents";
-console.log(fetch(api));
+const data = ["https://valorant-api.com/v1/agents"];
 
-const agent = document.querySelector("#displayName");
+const filterAPI = data
+  .filter((data) => data.displayName === `${DOMSelectors.input}`)
+  .forEach((data) => {
+    console.log(data.displayName);
+  });
+
+/* const agent = document.querySelector("#displayName");
 const AgentSearch = `${api}${agent}`;
-console.log(AgentSearch);
+console.log(AgentSearch); */
 
 // fetch() returns a "response", which we must convert into a object json format
-fetch(api)
+fetch(data)
   .then((response) => response.json())
   .then((data) => console.log(data));
 
 async function fetchData(api) {
   try {
-    const response = await fetch(api);
-    const data = await response.json();
-    console.log(data);
-    return data;
+    const response = await fetch(data);
+    const api = await response.json();
+    console.log(api);
+    return api;
   } catch (error) {
+    const apiResponseDOM = document.getElementById("api-response");
+    apiResponseDOM.innerHTML = `<h2>Error has occured. Make sure spelling is correct.</h2>`;
     console.error(error);
   }
 }
-fetchData(api);
+fetchData(data);
 
 DOMSelectors.button.addEventListener("click", function (event) {
   const apiResponseDOM = document.getElementById("api-response");
   const putcharacterInHTML = async () => {
-    const character = await fetchData(api);
+    const character = await fetchData(data);
     apiResponseDOM.innerHTML = ` <div class="card-info" id="card">
-  <h2 class="AgentName"> Agent: ${api.displayName} </h2>
-  <img src="${api.bustPortrait}" alt="Image of Valorant Agent">
-  <q class="Description"> Description: ${api.description}</q>
+  <h2 class="AgentName"> Agent: ${data.displayName} </h2>
+  <img src="${data.bustPortrait}" alt="Image of Valorant Agent">
+  <q class="Description"> Description: ${data.description}</q>
   </div>`;
   };
   putcharacterInHTML();
